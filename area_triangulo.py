77e7eec905c65e4dc616348c8bbec674b79d653b
raffaello64 <rafael.jimenez107@comunidadunir.net>
2022-08-16 12:34:21 +0200

    Área triángulo y círculo
    Programa en Python para hallar el área de un triángulo y un círculo, teniendo en cuenta los datos ingresados por el usuario

--- area_triangulo.py
+++ area_triangulo.py
@@ -7,3 +7,12 @@ altura = float(input('Introduzca el área de la altura: '))
 
 print('El área del triángulo es: ', area_triangulo(base, altura))
 
+# Ejercicio 2
+
+def area_circulo(radio):
+    area_total_circulo = 3.14 * (radio ** 2)
+    return area_total_circulo
+
+radio = float(input('Introduzca el radio del círculo: '))
+print('El área del círculo es: ', area_circulo(radio))
+
